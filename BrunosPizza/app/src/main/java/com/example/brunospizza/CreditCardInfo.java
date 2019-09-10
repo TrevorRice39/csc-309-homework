2cd807c20efdf91ed3ec7f2197cbcb3f9f8340d1
Trevor Rice <trevor_rice39@mymail.eku.edu>
2019-09-10 08:17:17 -0400

    added button functionality, created horizontal layout

---
+++ BrunosPizza/app/src/main/java/com/example/brunospizza/CreditCardInfo.java
@@ -0,0 +1,19 @@
+package com.example.brunospizza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CreditCardInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_card_info);
+
+
+    }
+}
