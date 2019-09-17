8e5bb9514009b18be00332f5dc51817fbd667691
Trevor Rice <trevor_rice39@mymail.eku.edu>
2019-09-17 11:43:47 -0400

    Replaced radio buttons with switches, added some listeners

--- ClassScheduler/app/src/main/java/com/example/classscheduler/CreateCourse.java
+++ ClassScheduler/app/src/main/java/com/example/classscheduler/CreateCourse.java
@@ -3,6 +3,15 @@ package com.example.classscheduler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateCourse extends AppCompatActivity {
 
@@ -10,5 +19,46 @@ public class CreateCourse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_course);
+
+        // radio buttons for the days of the week
+        Switch sw_monday = findViewById(R.id.sw_monday);
+        Switch sw_tuesday = findViewById(R.id.sw_tuesday);
+        Switch sw_wednesday = findViewById(R.id.sw_wednesday);
+        Switch sw_thursday = findViewById(R.id.sw_thursday);
+        Switch sw_friday = findViewById(R.id.sw_friday);
+        List<Switch> swList = new ArrayList<Switch>();
+        swList.add(sw_monday);
+        swList.add(sw_tuesday);
+        swList.add(sw_wednesday);
+        swList.add(sw_thursday);
+        swList.add(sw_thursday);
+
+        // buttons for selecting the time frame
+        Button btn_start_time = findViewById(R.id.btn_start_time);
+        Button btn_end_time = findViewById(R.id.btn_end_time);
+
+        btn_start_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btn_end_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // submit your changes button
+        Button btn_submit = findViewById(R.id.btn_submit);
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
--- ClassScheduler/app/src/main/java/com/example/classscheduler/MainActivity.java
+++ ClassScheduler/app/src/main/java/com/example/classscheduler/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.classscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    protected static final int SETTINGS_REQUEST_CODE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // buttons to add a class or view your current schedule
+        Button btn_add_class = findViewById(R.id.btn_add_class);
+        Button btn_view_schedule = findViewById(R.id.btn_view_schedule);
+
+        btn_add_class.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createCourseIntent = new Intent(
+                        getApplicationContext(), CreateCourse.class );
+                // settingsIntent.putExtra();
+                // start the activity, getting a response
+                startActivityForResult( createCourseIntent, SETTINGS_REQUEST_CODE );
+            }
+        });
     }
 }
