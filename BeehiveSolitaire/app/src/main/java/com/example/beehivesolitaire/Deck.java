916e66d6204cdaa4facba7623e189863473c82cd
Trevor Rice <trevor_rice39@mymail.eku.edu>
2019-10-18 21:59:39 -0400

    started implementing the on click listeners for the image views

--- BeehiveSolitaire/app/src/main/java/com/example/beehivesolitaire/Deck.java
+++ BeehiveSolitaire/app/src/main/java/com/example/beehivesolitaire/Deck.java
@@ -1,7 +1,6 @@
 package com.example.beehivesolitaire;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class Deck {
--- BeehiveSolitaire/app/src/main/java/com/example/beehivesolitaire/MainActivity.java
+++ BeehiveSolitaire/app/src/main/java/com/example/beehivesolitaire/MainActivity.java
@@ -2,16 +2,42 @@ package com.example.beehivesolitaire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    // creating a new deck
+    Deck deck = new Deck();
+    boolean cardSelected = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // creating a new deck
-        Deck deck = new Deck();
+        final ImageView garden0 = findViewById(R.id.iv_garden0);
+        garden0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (cardSelected) {
+                    garden0.setColorFilter(Color.TRANSPARENT);
+                }
+                else {
+                    garden0.setColorFilter(Color.LTGRAY, PorterDuff.Mode.DARKEN);
+                }
+                cardSelected = !cardSelected;
+            }
+        });
 
         // all possible ranks and suits
         char[] ranks = {'2', '3', '4', '5', '6', '7', '8', '9', 't', 'j', 'q', 'k', 'a'};
@@ -30,6 +56,26 @@ public class MainActivity extends AppCompatActivity {
         }
 
         deck.newGame();
+        updateCards();
+    }
+
+    private void updateCards() {
+        ImageView garden0 = findViewById(R.id.iv_garden0);
+        ImageView garden1 = findViewById(R.id.iv_garden1);
+        ImageView garden2 = findViewById(R.id.iv_garden2);
+        ImageView garden3 = findViewById(R.id.iv_garden3);
+        ImageView garden4 = findViewById(R.id.iv_garden4);
+        ImageView garden5 = findViewById(R.id.iv_garden5);
+
+        garden0.setImageResource(deck.garden[0].id);
+        garden1.setImageResource(deck.garden[1].id);
+        garden2.setImageResource(deck.garden[2].id);
+        garden3.setImageResource(deck.garden[3].id);
+        garden4.setImageResource(deck.garden[4].id);
+        garden5.setImageResource(deck.garden[5].id);
+
+        ImageView beehive = findViewById(R.id.iv_beehive);
+        beehive.setImageResource(deck.pack.get(deck.pack.size() - 1).id);
 
     }
 }
